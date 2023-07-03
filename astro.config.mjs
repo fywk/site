767da19c13ee22de775bfcfa00b98b0a1d5fcc1a
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-07-03 18:49:48 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://fywk.xyz",
+  compressHTML: true,
   integrations: [
     tailwind({
       applyBaseStyles: false,
--- prettier.config.js
+++ prettier.config.js
@@ -11,6 +11,7 @@ module.exports = {
       },
     },
   ],
+  pluginSearchDirs: false,
   // prettier-plugin-astro
   astroAllowShorthand: true,
 };
